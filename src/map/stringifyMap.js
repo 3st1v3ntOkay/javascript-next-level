e7a2229b6cdb69e2227488ae3111ef50ba72f941
Esteban Jimenez Valdez <wiwiwixd732@gmail.com>
2023-10-22 13:30:46 -0700

    feat: nothing special

--- src/map/stringifyMap.js
+++ src/map/stringifyMap.js
@@ -1,37 +1,37 @@
 // ? Luego pasarlo a Typescript
-const myMap = new Map()
+const myMap = new Map();
 
 const setValuesToMap = (key, value) => {
-  return myMap.set(key, value)
-}
+  return myMap.set(key, value);
+};
 
-setValuesToMap('a', 1)
-setValuesToMap('b', 2)
-setValuesToMap('c', 3)
-setValuesToMap('d', 4)
+setValuesToMap("a", 1);
+setValuesToMap("b", 2);
+setValuesToMap("c", 3);
+setValuesToMap("d", 4);
 
 const stringifyMap = (map) => {
-  let mapToJSON = JSON.stringify(Array.from(map.entries()))
-  return mapToJSON
-}
+  let mapToJSON = JSON.stringify(Array.from(map.entries()));
+  return mapToJSON;
+};
 
-const stringified = stringifyMap(myMap)
-// console.log(stringified)
+const stringified = stringifyMap(myMap);
+console.log(stringified);
 
 const parseMap = (mapJSON) => {
-  let parseToJSON = new Map(JSON.parse(mapJSON))
-  return parseToJSON
-}
+  let parseToJSON = new Map(JSON.parse(mapJSON));
+  return parseToJSON;
+};
 
-const parsed = parseMap(stringified)
-// console.log(parsed)
+const parsed = parseMap(stringified);
+console.log(parsed);
 
 const updateNumberValue = (map, key, replace = 1) => {
-  const value = map.get(key)
-  return map.set(key, value + replace)
-}
+  const value = map.get(key);
+  return map.set(key, value + replace);
+};
 
-updateNumberValue(myMap, 'a', 3)
-updateNumberValue(myMap, 'a', 3)
+updateNumberValue(myMap, "a", 3);
+updateNumberValue(myMap, "a", 3);
 
-// console.log(myMap);
+console.log(myMap);
